a601543e9373b9f5ea1c787205adff96236309d2
Cemile <cemile.balkas@sunderlandsoftwarecity.com>
2024-08-20 15:13:32 +0100

    added if else statement

--- public/app.js
+++ public/app.js
@@ -1,11 +1,34 @@
-"use strict";
-const form = document.querySelector('.new-item-form');
-const type = document.querySelector('#type');
-const toFrom = document.querySelector('#tofrom');
-const details = document.querySelector('#details');
-const amount = document.querySelector('#amount');
-form.addEventListener('submit', (e) => {
+import { Invoice } from "./classes/Invoice.js";
+import { Payment } from "./classes/Payment.js";
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
+// docOne = new Invoice("Anna", "website work", 200);
+// docTwo = new Payment("Simon", "video editing", 640);
+// let docs: HasFormatter[] = [];
+// docs.push(docOne);
+// docs.push(docTwo);
+// console.log(docs);
+// const invOne = new Invoice("Cemile", "work on her garden", 400);
+// const invTwo = new Invoice("Dami", "work on her house", 480);
+// let invoices: Invoice[] = [];
+// invoices.push(invOne);
+// invoices.push(invTwo);
+// invoices.forEach((inv) => {
+//   console.log(inv.client, inv.amount, inv.format())
+// });
+const form = document.querySelector(".new-item-form");
+const type = document.querySelector("#type");
+const toFrom = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log(type);
-    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+    }
+    console.log(doc);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,46 @@
+import { Invoice } from "./classes/Invoice.js";
+import { Payment } from "./classes/Payment.js";
+import { HasFormatter } from "./interfaces/HasFormatter.js";
 
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
 
-const form = document.querySelector('.new-item-form') as HTMLFormElement;
+// docOne = new Invoice("Anna", "website work", 200);
+// docTwo = new Payment("Simon", "video editing", 640);
 
+// let docs: HasFormatter[] = [];
+// docs.push(docOne);
+// docs.push(docTwo);
 
-const type = document.querySelector('#type') as HTMLSelectElement;
-const toFrom = document.querySelector('#tofrom') as HTMLInputElement;
-const details = document.querySelector('#details') as HTMLInputElement;
-const amount = document.querySelector('#amount') as HTMLInputElement;
+// console.log(docs);
 
-form.addEventListener('submit', (e: Event) => {
-    e.preventDefault();
-    console.log(type);
-    console.log(
-        type.value, 
-        toFrom.value, 
-        details.value, 
-        amount.valueAsNumber
-      );
+// const invOne = new Invoice("Cemile", "work on her garden", 400);
+// const invTwo = new Invoice("Dami", "work on her house", 480);
+
+// let invoices: Invoice[] = [];
+
+// invoices.push(invOne);
+// invoices.push(invTwo);
+
+// invoices.forEach((inv) => {
+//   console.log(inv.client, inv.amount, inv.format())
+
+// });
+
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+
+const type = document.querySelector("#type") as HTMLSelectElement;
+const toFrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+  let doc: HasFormatter;
+  if (type.value === 'invoice') {
+    doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber)
+  } else {
+    doc = new Payment(toFrom.value, details.value, amount.valueAsNumber)
+  }
+  console.log(doc);
 });
